7fee84cd27a986c843e31f926e2b1df3ce404f97
chiricos <edwarddiaz92@gmail.com>
2015-03-25 13:03:36 -0500

    header

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Unidad de licores del meta</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href='css/normalize.min.css'/>
+    <link rel="stylesheet" href='css/style.css'/>
+
+    <script src='js/script.js'></script>
+
+    <!-- Humans
+    <link type="text/plain" rel="author" href="{{asset('humans.txt')}}"/>-->
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+    <header>
+        <section class="section-header-up">
+        </section>
+        <section class="menu-header">
+            <figure>
+                <img src="images/logo.png" />
+            </figure>
+            <div class="content-menu">
+                <nav>
+                    <a href="">INICIO</a>
+                    <a href="">LA EMPRESA</a>
+                    <a href="">PRODUCTOS</a>
+                    <a href="">DISTRIBUIDORES</a>
+                    <a href="">CONTÁCTENOS</a>
+                </nav>
+            </div>
+        </section>
+    </header>
+
+    <section class="content-social">
+        <img src="images/facebook.png">
+        <img src="images/twitter.png">
+        <img src="images/youtube.png">
+    </section>
+
+    <div class="wrapper">
+
+    </div>
+</body>
+</html>
